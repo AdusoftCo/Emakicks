6186264662f20cac7130d8dda447a425710d4b78
Oscar <oscar_aduviri@yahoo.com>
2025-11-13 11:54:13 -0300

    leeters in home

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -35,7 +35,7 @@ const Navegacion = () => {
                             alt="Icono del Comercio"
                             style={{ width: '48px', height: '48px', marginRight: '15px' }}
                             />
-                            <span className="fw-bold text-dark fs-5 cursive" style={{ color: '#0a3d62' }}>EMAKICK'S</span>
+                            <span className="text-dark fs-5 cursive" style={{ color: '#0a3d62' }}>EMAKICK'S</span>
                         </Navbar.Brand>
 
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
