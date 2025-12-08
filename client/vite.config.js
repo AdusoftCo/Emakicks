3647be66d0cbe1848fc3c08762d36865437f6efb
Oscar <oscar_aduviri@yahoo.com>
2025-12-08 14:42:43 -0300

    rail 8

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: process.env.PORT || 4173,
+    host: '0.0.0.0',
+    allowedHosts: [
+      'emakicks-frontend-production-4e1b.up.railway.app'
+    ]
+  }
 })
