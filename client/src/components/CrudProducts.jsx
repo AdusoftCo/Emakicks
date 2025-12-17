5dba4ae0cd99e007861f204240b067fe2aca31e3
Oscar <oscar_aduviri@yahoo.com>
2025-12-17 11:29:20 -0300

    crud fabricants

--- client/src/components/CrudProducts.jsx
+++ client/src/components/CrudProducts.jsx
@@ -414,9 +414,9 @@ const CrudProducts = () => {
                             <tr key={product.id}>
                                 <td>{product.cod_art}</td>
                                 <td>
-                                {product.imagen_base64 ? (
+                                {product.imagen_url ? (
                                     <img
-                                        src={`data:image/jpeg;base64,${product.imagen_base64}`}
+                                        src={`data:image/jpeg;base64,${product.imagen_url}`}
                                         alt={product.descripcion}
                                         style={{ width: '64px', height: '64px', objectFit: 'cover' }}
                                         onError={(e) => {
